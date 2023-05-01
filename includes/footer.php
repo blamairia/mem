1dc59caec9d95a5f517071ef7f84b6ad86bb194e
blamairia <blamairia@gmail.com>
2023-05-01 20:12:29 +0100

    added the about and services

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,17 @@
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/parallax/parallax.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
 
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="assets/js/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-<script src="assets/js/custom.js"></script>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 <!--Alertify Js -->
 <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
     
--- index.php
+++ index.php
@@ -47,38 +47,6 @@ if (isset($_GET['sortby'])) {
 
 
 ?>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="assets/js/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-<script src="assets/js/custom.js"></script>
-<!--Alertify Js -->
-<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
-    
-    
-    <script>
-        alertify.set('notifier','position', 'top-right');
-        <?php if(isset($_SESSION['message'])) 
-        { 
-         
-         ?>
-         alertify.success('<?= $_SESSION['message'];?>');
-        <?php 
-                    unset($_SESSION['message']);
-        } 
-        ?>
-    </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-<!-- MDB -->
-<script
-  type="text/javascript"
-  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.js"
-></script>
-
 
 <!-- Carousel Start -->
 <div class="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s">
@@ -173,130 +141,118 @@ if (isset($_GET['sortby'])) {
         </div>
     </div>
     <!-- Top Feature End -->
-<div class="py-3 bg-dark mb-0">
-    <div class="container">
-        <a href="index.php" class="text-white" style="text-decoration:none">Home / </a><a href="categories.php" class="text-white" style="text-decoration:none" >Collections</a>
-        <div class="dropdown">
-                <button class="dropbtn">Categories</button>
-                <div class="dropdown-content">
-                <?php 
-                                        $categories = getAll("categories");
-
-                                        if(mysqli_num_rows($categories) > 0)
-                                        {
-                                            foreach($categories as $item)
-                                            {
-                                                ?>
-                                                     <a href="products.php?category=<?= $item['slug']; ?>">
-                                                        <option value="<?= $item['id']; ?>"><?= $item['name']; ?></option>
-                                                    </a>
-                                                <?php
-                                            }
-                                        }
-                                        else
-                                        {
-                                            echo "No Category Available";
-                                        }
-                                    ?>
+< <!-- About Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5 align-items-end">
+                <div class="col-lg-3 col-md-5 wow fadeInUp" data-wow-delay="0.1s">
+                    <img class="img-fluid rounded" data-wow-delay="0.1s" src="img/about.jpg">
                 </div>
-        </div>
-       
-    </div>
-</div>        
-        
-        
-<div class="py-5 container mb-0">
-
-    <div class="row">
-        <div class="col-md-12">
-                <?php   
-                    if(isset($_SESSION['message'])) { 
-                        ?>
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                            <strong>Hey! </strong><?= $_SESSION['message']; ?>.
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <div class="col-lg-6 col-md-7 wow fadeInUp" data-wow-delay="0.3s">
+                    <h1 class="display-1 text-primary mb-0">25</h1>
+                    <p class="text-primary mb-4">Year of Experience</p>
+                    <h1 class="display-5 mb-4">We Make Your Home Like A Garden</h1>
+                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                    <a class="btn btn-primary py-3 px-4" href="">Explore More</a>
+                </div>
+                <div class="col-lg-3 col-md-12 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="row g-5">
+                        <div class="col-12 col-sm-6 col-lg-12">
+                            <div class="border-start ps-4">
+                                <i class="fa fa-award fa-3x text-primary mb-3"></i>
+                                <h4 class="mb-3">Award Winning</h4>
+                                <span>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna</span>
+                            </div>
                         </div>
-
-                    
-                    <?php 
-                    unset($_SESSION['message']);
-                    }
-                        ?>
-        </div>
-    </div>
-    <div class="row mt-0">
-	    	
-                
-
-
-
-    <div class="row m-3 mb-5 shadow-lg p-3 mb-5 bg-white rounded">
-        <div class="col-sm-8">
-            <div class="dropdown ">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                    Sort By defoault
-                </button>
-                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                <li><a class="price asc dropdown-item"  href="<?php echo $index . "?sortby=asc" ?>">Asending Price</a></li>
-                    <li><a class="price dsc dropdown-item" href=<?php echo $index . "?sortby=dsc" ?>>Desending Price</a></li>
-                    
-                </ul>
+                        <div class="col-12 col-sm-6 col-lg-12">
+                            <div class="border-start ps-4">
+                                <i class="fa fa-users fa-3x text-primary mb-3"></i>
+                                <h4 class="mb-3">Dedicated Team</h4>
+                                <span>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-sm-4">
-            <form class="form" action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="POST">
-                <div class="input-group">
-                    
-                            <div class="form-outline">
-                                <input name="search" value="<?php echo $search ?>" type="search" class="form-control" />
-                                <label class="form-label" for="search">Search</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            </div>
-                    
-                </div>
-            </form>
-        </div>
-    <div class="container mt-3 bg-light product-data">
-                <div class="row">
-                <?php  foreach($products as $item) :?>
-                    
-                        
-                            <div class="col-md-4 mb-2">
-                                            <a style="text-decoration:none;" href="product-view.php?product=<?= $item['slug']; ?>">
-                                                <div class="card shadow">
-                                                    <div class="card-body">
-                                                        <img src="uploads/<?= $item['image']; ?>" alt="category image" class="w-100">
-                                                        <h4  class="text-center"><?= $item['name']; ?></h4>
-                                                        
-                                                        <H4 class ="text-center">Price : $ <span class="text-success fw-bold"> <?= $item['selling_price']; ?></span></H4>
-                                                        <div class="justify-content-center row">
-
-                                                       
-                                                        
-                                                        
-                                            
-
-                                                        <button class="btn bg-white addToCart-btn btn-primary text-center" value="<?= $item['id'] ?>" type="button"><i class="fa fa-shopping-cart me-2"></i>Add to cart</a></button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                            
-                                        
-                            </div>     
-                                  
-                        <?php  endforeach; ?>
-                            
-               
-
-
-                    
-                </div>
     </div>
-</div>
+    <!-- About End -->
+
+
+    <!-- Facts Start -->
+    <div class="container-fluid facts my-5 py-5" data-parallax="scroll" data-image-src="img/carousel-1.jpg">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
+                    <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
+                    <span class="fs-5 fw-semi-bold text-light">Happy Clients</span>
                 </div>
+                <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
+                    <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
+                    <span class="fs-5 fw-semi-bold text-light">Garden Complated</span>
+                </div>
+                <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
+                    <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
+                    <span class="fs-5 fw-semi-bold text-light">Dedicated Staff</span>
+                </div>
+                <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
+                    <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
+                    <span class="fs-5 fw-semi-bold text-light">Awards Achieved</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Facts End -->
+
+
+    <!-- Features Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <p class="fs-5 fw-bold text-primary">Why Choosing Us!</p>
+                    <h1 class="display-5 mb-4">Few Reasons Why People Choosing Us!</h1>
+                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                    <a class="btn btn-primary py-3 px-4" href="">Explore More</a>
+                </div>
+                <div class="col-lg-6">
+                    <div class="row g-4 align-items-center">
+                        <div class="col-md-6">
+                            <div class="row g-4">
+                                <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
+                                    <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
+                                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
+                                            <i class="fa fa-check fa-3x text-primary"></i>
+                                        </div>
+                                        <h4 class="mb-0">100% Satisfaction</h4>
+                                    </div>
+                                </div>
+                                <div class="col-12 wow fadeIn" data-wow-delay="0.5s">
+                                    <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
+                                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
+                                            <i class="fa fa-users fa-3x text-primary"></i>
+                                        </div>
+                                        <h4 class="mb-0">Dedicated Team</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 wow fadeIn" data-wow-delay="0.7s">
+                            <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
+                                <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
+                                    <i class="fa fa-tools fa-3x text-primary"></i>
+                                </div>
+                                <h4 class="mb-0">Modern Equipment</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+    <!-- Features End -->
+        
+
 
 <?php include('includes/footer.php'); ?>
